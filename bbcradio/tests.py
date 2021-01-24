f2272a2ec58cc7784f66f72d1d4c306fc9e0e537
Steven Maude <git@stevenmaude.co.uk>
2021-01-24 22:30:05 +0000

    Add tests for `Schedule` and `Programme`

--- bbcradio/tests.py
+++ bbcradio/tests.py
@@ -97,3 +97,302 @@ class TestStations(unittest.TestCase):
         )
 
 
+class TestSchedule(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        path = pathlib.Path("fixtures") / "schedule.html"
+        with open(path, "r") as f:
+            page_element = html.fromstring(f.read())
+
+        cls.programmes = bbcradio.Schedule._extract(page_element)
+
+    def test_construct_url(self):
+        station = bbcradio.Station(
+            "BBC Unittest Station", "https://example.com/unittest"
+        )
+        test_date = "2021-01-24"
+        schedule = bbcradio.Schedule(station, test_date)
+        self.assertEqual(
+            "https://example.com/unittest/2021/01/24",
+            schedule._construct_url(),
+        )
+
+    def test_correct_extracted_programmes(self):
+        self.maxDiff = None
+        expected_programmes = [
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T00:00:00+00:00",
+                    "series_name": "Radio 1's Essential Mix",
+                    "name": "Vintage Culture",
+                    "description": "The Brazilian superstar DJ takes control of the Essential Mix decks.",
+                    "identifier": "m000rcdj",
+                    "url": "https://www.bbc.co.uk/programmes/m000rcdj",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T02:00:00+00:00",
+                    "series_name": "Radio 1 Dance Presents...",
+                    "name": "DJ Mag: Nightwave",
+                    "description": "DJ Mag takes us to Scotland for an hour of bass, techno and blistering acid with Nightwave",
+                    "identifier": "m000rcdn",
+                    "url": "https://www.bbc.co.uk/programmes/m000rcdn",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T03:00:00+00:00",
+                    "series_name": "Annie Mac in the Mix",
+                    "name": "Piano House!",
+                    "description": "Annie celebrates all things Piano house, old and new!",
+                    "identifier": "m000rcds",
+                    "url": "https://www.bbc.co.uk/programmes/m000rcds",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T03:30:00+00:00",
+                    "series_name": "Annie Mac in the Mix",
+                    "name": "House and Disco!",
+                    "description": "Annie serves up another special mix.",
+                    "identifier": "m000q2lm",
+                    "url": "https://www.bbc.co.uk/programmes/m000q2lm",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T04:00:00+00:00",
+                    "series_name": "Radio 1's Wind Down Presents...",
+                    "name": "Intergral Records: Phil.Osophy",
+                    "description": "Music designed to unwind the mind.",
+                    "identifier": "m000rcdx",
+                    "url": "https://www.bbc.co.uk/programmes/m000rcdx",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T05:00:00+00:00",
+                    "series_name": "The Happy Hour from Radio 1",
+                    "name": "Feel Good Happy Tunes!",
+                    "description": "Feel good and happy tunes that will keep you smiling during lockdown life!",
+                    "identifier": "m000rdqj",
+                    "url": "https://www.bbc.co.uk/programmes/m000rdqj",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T06:00:00+00:00",
+                    "series_name": "Radio 1 Dance",
+                    "name": "24/7 Dance...",
+                    "description": "Classic hits and the best new dance tracks.",
+                    "identifier": "m000rl69",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl69",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T07:00:00+00:00",
+                    "series_name": "Adele Roberts",
+                    "name": "23/01/2021",
+                    "description": "Adele Roberts takes charge of your weekend wake-up...",
+                    "identifier": "m000rl6c",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl6c",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T10:00:00+00:00",
+                    "series_name": "Radio 1 Anthems",
+                    "name": "with Adele Roberts",
+                    "description": "Big anthems and tunes you haven't heard in ages!",
+                    "identifier": "m000rl6f",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl6f",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T10:32:00+00:00",
+                    "series_name": "Radio 1 Anthems",
+                    "name": "with Jordan North",
+                    "description": "Big anthems and tunes you haven't heard in ages!",
+                    "identifier": "m000rl6h",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl6h",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T11:00:00+00:00",
+                    "series_name": "Jordan North",
+                    "name": "23/01/2021",
+                    "description": "Big hits and the best new music.",
+                    "identifier": "m000rl6k",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl6k",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T13:00:00+00:00",
+                    "series_name": "Matt and Mollie",
+                    "name": "23/01/2021",
+                    "description": "Afternoon fun and games with Matt Edmondson and Mollie King",
+                    "identifier": "m000rl6m",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl6m",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T16:00:00+00:00",
+                    "series_name": "Radio 1's Dance Anthems",
+                    "name": "Classic Dance Anthems with Charlie Hedges",
+                    "description": "Charlie crosses the spectrum of Dance music with tracks from Mella Dee, Otto Knows & Mylo.",
+                    "identifier": "m000rl6p",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl6p",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T17:00:00+00:00",
+                    "series_name": "Radio 1's Dance Anthems",
+                    "name": "Classic Dance Anthems with Charlie Hedges",
+                    "description": "Charlie continues the party with anthems from Patrick Topping, Weiss and The Prodigy.",
+                    "identifier": "m000rl6r",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl6r",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T18:00:00+00:00",
+                    "series_name": "Radio 1's Dance Anthems",
+                    "name": "Today's Dance Anthems with Charlie Hedges",
+                    "description": "Charlie mixes up the biggest Dance Anthems.",
+                    "identifier": "m000rl6t",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl6t",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T19:00:00+00:00",
+                    "series_name": "1Xtra's Takeover with DJ Target",
+                    "name": "Kenny sits in for Target with a 50 Cent Versus Mix",
+                    "description": "Kenny Allstar is in for Target as 1Xtra takes over Radio 1!",
+                    "identifier": "m000rl6w",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl6w",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T21:00:00+00:00",
+                    "series_name": "1Xtra's Rap Show with Tiffany Calver",
+                    "name": "Fredo Street Heat",
+                    "description": "All the latest hits and heat from the world of Rap plus Fredo is this weeks street heat.",
+                    "identifier": "m000rl6y",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl6y",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-23T23:00:00+00:00",
+                    "series_name": "Diplo and Friends",
+                    "name": "Diplo in the Mix",
+                    "description": "Diplo in the mix exclusively for Diplo and friends - only on Radio 1 and 1Xtra.",
+                    "identifier": "m000rl70",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl70",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-24T01:00:00+00:00",
+                    "series_name": "Radio 1's Classic Essential Mix",
+                    "name": "Four Tet 2010",
+                    "description": "Relive Four Tet's Essential Mix debut from 2010.",
+                    "identifier": "m000rl74",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl74",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-24T03:00:00+00:00",
+                    "series_name": "Danny Howard's Club Mix",
+                    "name": "Episode 2",
+                    "description": "Danny goes in with a another Feel Good Club Mix.",
+                    "identifier": "m000rl76",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl76",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-24T04:00:00+00:00",
+                    "series_name": "Radio 1's Dance Anthems",
+                    "name": "Classic Dance Anthems with Charlie Hedges",
+                    "description": "Non-stop Classic Dance Anthems with Charlie!",
+                    "identifier": "m000rl78",
+                    "url": "https://www.bbc.co.uk/programmes/m000rl78",
+                }
+            ),
+            bbcradio.Programme(
+                **{
+                    "start_date": "2021-01-24T05:00:00+00:00",
+                    "series_name": "Radio 1's Wind Down Presents...",
+                    "name": "Integral: Emma G & MC Tali",
+                    "description": "Integral's Emma G & MC Tali provide the Wind Down Mix.",
+                    "identifier": "m000qkz2",
+                    "url": "https://www.bbc.co.uk/programmes/m000qkz2",
+                }
+            ),
+        ]
+
+        self.assertEqual(expected_programmes, self.programmes)
+
+    def test_create_with_valid_date(self):
+        station = bbcradio.Station(
+            "BBC Unittest Station", "https://example.com/unittest"
+        )
+        test_date = "2021-01-24"
+        schedule = bbcradio.Schedule(station, test_date)
+        self.assertEqual("BBC Unittest Station", schedule.station.name)
+        self.assertEqual("https://example.com/unittest", schedule.station.url)
+        self.assertIsNone(schedule._programmes)  # Avoid really computing.
+        self.assertEqual(test_date, schedule.date)
+
+    def test_create_with_invalid_date(self):
+        station = bbcradio.Station(
+            "BBC Unittest Station", "https://example.com/unittest"
+        )
+        self.assertRaises(ValueError, bbcradio.Schedule, station, "not-a-date")
+
+
+class TestProgramme(unittest.TestCase):
+    def test_create_with_all_valid_values(self):
+        info = {
+            "start_date": "2021-01-24T05:00:00+00:00",
+            "series_name": "Radio 1's Wind Down Presents...",
+            "name": "Integral: Emma G & MC Tali",
+            "description": "Integral's Emma G & MC Tali provide the Wind Down Mix.",
+            "identifier": "m000qkz2",
+            "url": "https://www.bbc.co.uk/programmes/m000qkz2",
+        }
+        programme = bbcradio.Programme(**info)
+
+        expected_programme = bbcradio.Programme()
+        expected_programme._info = info
+
+        self.assertEqual(expected_programme, programme)
+
+    def test_create_with_some_invalid_values(self):
+        original_info = {
+            "start_date": "2021-01-24T05:00:00+00:00",
+            "series_name": "Radio 1's Wind Down Presents...",
+            "name": "Integral: Emma G & MC Tali",
+            "description": "Integral's Emma G & MC Tali provide the Wind Down Mix.",
+            "identifier": "m000qkz2",
+            "url": "https://www.bbc.co.uk/programmes/m000qkz2",
+        }
+        info = original_info.copy()
+        info["nonhandled_info"] = "Some unnecessary information"
+        programme = bbcradio.Programme(**info)
+
+        expected_programme = bbcradio.Programme()
+        expected_programme._info = original_info
+
+        self.assertEqual(expected_programme, programme)
