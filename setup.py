478c1526ac38e21dc3f5f88a677b6b3cc699b896
Steven Maude <git@stevenmaude.co.uk>
2021-01-27 18:48:42 +0000

    Tidy setup.py
    Remove comments from original.

--- setup.py
+++ setup.py
@@ -39,60 +39,9 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
     ],
     keywords="bbc, radio, schedule",
-    # When your source code is in a subdirectory under the project root, e.g.
-    # `src/`, it is necessary to specify the `package_dir` argument.
-    # package_dir={'': 'src'},  # Optional
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
-    # packages=find_packages(where='src'),  # Required
-    packages=find_packages(),  # Required
+    packages=find_packages(),
     python_requires=">=3.6, <4",
     install_requires=["lxml~=4.6.2", "requests~=2.25.1"],
-    extras_require={
-        "dev": ["black==20.8b1"],
-        #'test': ['coverage'],
-    },
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    # package_data={
-    #    'sample': ['package_data.dat'],
-    # },
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/distutils/setupscript.html#installing-additional-files
-    #
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],  # Optional
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    entry_points={  # Optional
-        "console_scripts": ["bbcradio_cli=bbcradio.cli:main",],
-    },
-    # List additional URLs that are relevant to your project as a dict.
-    #
-    # This field corresponds to the "Project-URL" metadata fields:
-    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
-    #
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
-    # project_urls={  # Optional
-    #   'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
-    #   'Funding': 'https://donate.pypi.org',
-    #   'Say Thanks!': 'http://saythanks.io/to/example',
-    #   'Source': 'https://github.com/pypa/sampleproject/',
-    # },
+    extras_require={"dev": ["black==20.8b1"],},
+    entry_points={"console_scripts": ["bbcradio_cli=bbcradio.cli:main",],},
 )
